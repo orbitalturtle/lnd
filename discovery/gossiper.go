545fe43b5b9ea7dff2edb743a30be25389b82da2
Olaoluwa Osuntokun <laolu32@gmail.com>
2021-04-09 14:38:32 -0700

    discovery: always add chan announcements to the reject cache if err != ErrIgnored
    In this commit, we make a change to always add chan announcements to the
    reject cache if we didn't reject them for already existing.  Without
    this change, if we end up rejecting a channel announcement say because
    the channel is already spent or the funding transaction doesn't exist,
    then we'll end up continually re-validating the same set of channels we
    know will fail, when they're sent to us by peers.
    
    Fixes #5191

--- discovery/gossiper.go
+++ discovery/gossiper.go
@@ -1742,6 +1742,10 @@ func (d *AuthenticatedGossiper) processNetworkAnnouncement(
 			} else {
 				log.Tracef("Router rejected channel "+
 					"edge: %v", err)
+
+				d.rejectMtx.Lock()
+				d.recentRejects[msg.ShortChannelID.ToUint64()] = struct{}{}
+				d.rejectMtx.Unlock()
 			}
 
 			nMsg.err <- err
